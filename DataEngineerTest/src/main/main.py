36918bb8e81a8d2991cc77fd475e3ed0f0cddd65
Leonardo_RA <leoengufmg@gmail.com>
2022-12-22 16:00:33 -0300

    some small changes

--- DataEngineerTest/src/main/main.py
+++ DataEngineerTest/src/main/main.py
@@ -53,67 +53,6 @@ def get_page(url):
     return requests.get(url, headers=headers)
 
 
-def parse_rows(table_rows):
-    parsed_rows = []
-
-    for table_row in table_rows:
-        parsed_row = []
-        el = table_row.xpath("./div")
-
-        none_count = 0
-
-        for rs in el:
-            try:
-                (text,) = rs.xpath('.//span/text()[1]')
-                parsed_row.append(text)
-            except ValueError:
-                parsed_row.append(np.NaN)
-                none_count += 1
-
-        if none_count < 4:
-            parsed_rows.append(parsed_row)
-
-    return pd.DataFrame(parsed_rows)
-
-
-def clean_data(df):
-    df = df.set_index(0)  # Set the index to the first column: 'Period Ending'.
-    df = df.transpose()  # Transpose the DataFrame, so that our header contains the account names
-
-    # Rename the "Breakdown" column to "Date"
-    cols = list(df.columns)
-    cols[0] = 'Date'
-    df = df.set_axis(cols, axis='columns', inplace=False)
-
-    numeric_columns = list(df.columns)[1::]  # Take all columns, except the first (which is the 'Date' column)
-
-    for column_index in range(1, len(df.columns)):  # Take all columns, except the first (which is the 'Date' column)
-        df.iloc[:, column_index] = df.iloc[:, column_index].str.replace(',', '')  # Remove the thousands separator
-        df.iloc[:, column_index] = df.iloc[:, column_index].astype(np.float64)  # Convert the column to float64
-
-    return df
-
-
-def scrape_table_second_attempt(url):
-    # Fetch the page that we're going to parse
-    page = get_page(url);
-
-    # Parse the page with LXML, so that we can start doing some XPATH queries
-    # to extract the data that we want
-    tree = html.fromstring(page.content)
-
-    # Fetch all div elements which have class 'D(tbr)'
-    table_rows = tree.xpath("//div[contains(@class, 'D(tbr)')]")
-
-    # Ensure that some table rows are found; if none are found, then it's possible
-    # that Yahoo Finance has changed their page layout, or have detected
-    # that you're scraping the page.
-
-    assert len(table_rows) > 0
-    df = parse_rows(table_rows)
-    df = clean_data(df)
-    return df
-
 
 def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
     '''
@@ -128,13 +67,12 @@ def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
     # Defining column name
     column_name = ["Ticker", "Field", "Value", "End Date", "Scrape Date"]
     data = []
-    for ticker in tickers:
+    for ticker in tickerslist:
         flag = 0
         # define list to store column name and scraped values
-        for tab in tabs:
+        for tab in tabslist:
             # Set the URL for the current ticker
             url = base_url.format(ticker, tab, ticker)
-            # print(url)
 
             # Make a request to the URL
             response = get_page(url)
@@ -142,9 +80,6 @@ def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
             soup = BeautifulSoup(response.text, "html.parser")
             # scrape date
             scrape_date = datetime.datetime.now().strftime('%m-%d-%Y')
-            # print(scrape_date)
-
-            # End date
 
             if (tab == "financials") and (flag == 0):
                 try:
@@ -152,7 +87,8 @@ def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
                     enddate = soup.find_all("div", class_="Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b)")
                     enddate = enddate[0].text.replace("/", "-")
                 except:
-                    print("An exception occurred")
+                    #print("An exception occurred")
+                    pass
             table_field = soup.find_all("div", class_="D(tbr) fi-row Bgc($hoverBgColor):h")
             # Find the table containing the financial data
             for i in table_field:
@@ -168,13 +104,12 @@ def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
                         # append value
                         value.append(j.text)
                     aux += 1
-                    # Append End Date
+                # Append End Date
                 value.append(enddate)
-                # value.append(table_EndDate.replace("/", "-"))
 
                 # Append Scrape Date
                 value.append(scrape_date)
-                #
+
                 # Create a DataFrame with custom column labels
                 data.append(value)
     df = pd.DataFrame(data, columns=column_name)
@@ -182,11 +117,7 @@ def scrape_table_first_attempt(baseurl, tickerslist, tabslist):
     return df
 
 if __name__ == "__main__":
-    # argparser = argparse.ArgumentParser()
-    # argparser.add_argument('ticker', help='')
-    # args = argparser.parse_args()
-    # ticker = args.ticker
-    # Set the tickers and fields to be scrapped
+
     logger.info('Starting Main program!')
     tickers = ["JNJ", "BRK.B", "JPM", "MMM", "ABBV", "DIS", "T", "PG", "LOW", "CI"]
     fields = ["Operating Income", "Net Income From Continuing Operations", "Retained Earnings", "Change In Cash",
